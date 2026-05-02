3f633b8f1dadeadabd8642e3263758b78f41c152
williamcardozooficial-collab <williamcardozooficial@gmail.com>
2026-05-02 01:43:25 -0300

    Add sw.js - service worker para PWA FlashDrop Motoboy

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'flashdrop-motoboy-v1';
+const urlsToCache = [
+  'https://flashdrop-frontend-six.vercel.app/motoboy.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
